b0d1980c367910164556e4a09468533b84651925
Isaac Kelly <isaacmkelly0@gmail.com>
2019-08-29 18:25:13 -0500

    minor changes, better layout for the code, added class toggle for form, ready to render

--- script.js
+++ script.js
@@ -10,24 +10,29 @@ function Book(author, title, pages, read){
 // console.log(hobbit.info());
 
 function addBookToLibrary(){
-  //TODO: retrieve form to get values for a new book
+  // TODO: retrieve form to get values for a new book
   let author = document.getElementById('author').value;
   let title = document.getElementById('title').value;
   let pages = document.getElementById('pages').value;
   let read = document.getElementById('read').checked;
+  // TODO: create new book object and add to library array
   let book = new Book(author, title, pages, read);
-  // for(let key in book){
-  //   console.log(key)
-  //   console.log(book[key])
-  // }
   myLibrary.push(book);
-
+  // TODO: reset form to blank slate
+  document.getElementById('author').value = '';
+  document.getElementById('title').value = '';
+  document.getElementById('pages').value = '';
+  toggleForm();
 }
 
 function render(){
-
+  // TODO: create visible versions of all books in library on html page
 }
 
 function toggleForm(){
-  
+  // TODO: turn form invisible
+  let tags = document.querySelectorAll('.form');
+  tags.forEach(function(e){
+    e.classList.toggle('invisible');
+  })
 }
