40f41f63df8155f01da0e965f95cfcf32a15eae7
Isaac Kelly <isaacmkelly0@gmail.com>
2019-08-29 14:52:40 -0500

    Added skeleton code, first objective is to figure out how to prompt user for input

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+const newBook = document.getElementById('add');
+let myLibrary = [];
+
 function Book(author, title, pages, read){
   this.author = author;
   this.title = title;
   this.pages = pages;
   this.read = read;
-  this.info = function(){
-    return this.title + ' by ' + this.author + ', ' + this.pages + ' pages, ' + this.read +'.';
-  }
+}
+// const hobbit = new Book('J.R.R. Tolkein', 'The Hobbit', 295, 'not read yet');
+// console.log(hobbit.info());
+
+function addBookToLibrary(){
+  // var author = prompt('Enter the book\'s author');
+  // var title = prompt('Enter the book\'s title');
+  // var pages = prompt('Enter the number of pages this book has');
+  // var read = prompt('Have you read this book?');
+  
+}
+
+function render(){
+
 }
